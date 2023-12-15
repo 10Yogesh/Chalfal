738094b4c4dc1c50d43ffdb5458a1d21d29997f2
10Yogesh <yogeshkharel2@gmail.com>
2023-12-15 11:05:36 +0545

    mute/unmute

--- frontend/src/pages/Room/Room.jsx
+++ frontend/src/pages/Room/Room.jsx
@@ -1,122 +1,100 @@
-import React, { useEffect, useState } from 'react';
-import { useWebRTC } from '../../hooks/useWebRTC';
-import { useSelector} from 'react-redux';
-import { useParams,  useNavigate } from 'react-router-dom';
-import styles from './Room.module.css';
-import { getRoom } from '../../http';
-
-
+import React, { useEffect, useState } from "react";
+import { useWebRTC } from "../../hooks/useWebRTC";
+import { useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import styles from "./Room.module.css";
+import { getRoom } from "../../http";
 
 const Room = () => {
+  const { id: roomId } = useParams();
   const user = useSelector((state) => state.auth.user);
-  const {id: roomId} = useParams();
+
+  const { clients, provideRef, handleMute } = useWebRTC(roomId, user);
+  const uniqueClients = [
+    ...new Map(clients.map((item) => [item["id"], item])).values(),
+  ];
+  const navigate = useNavigate();
   const [room, setRoom] = useState(null);
   const [isMute, setMute] = useState(true);
 
-  const {clients, provideRef, handleMute} = useWebRTC(roomId, user);
-  const uniqueClients = [...new Map(clients.map(item =>
-    [item["id"], item])).values()];
- 
+  useEffect(() => {
+    handleMute(isMute, user.id);
+  }, [isMute, handleMute, user.id]);
 
-  const navigate = useNavigate();
   const handManualLeave = () => {
-    navigate('/rooms');
+    navigate("/rooms");
   };
 
   useEffect(() => {
-    handleMute(isMute, user.id);
-  }, [isMute]);
-
- 
-  useEffect(() => {
     const fetchRoom = async () => {
-        
-        const { data } = await getRoom(roomId);
-        console.log(data)
-        setRoom((prev) => data);
+      const { data } = await getRoom(roomId);
+      setRoom((prev) => data);
     };
+
     fetchRoom();
- }, [roomId]);
+  }, [roomId]);
 
-
- const handleMuteClick = (clientId) => {
-  if (clientId !== user.id) {
-      return;
-  } 
-  setMute((prev) => !prev);
-};
-
+  const handleMuteClick = () => {
+    setMute((prev) => !prev);
+  };
 
   return (
-    
     <div>
       <div className="container">
         <button onClick={handManualLeave} className={styles.goBack}>
-              <img src="/images/arrow-left.png" alt="arrow-left" />
-              <span>All voice rooms</span>
+          <img src="/images/arrow-left.png" alt="arrow-left" />
+          <span>All voice rooms</span>
         </button>
       </div>
-
-        {room && 
-         <div  className={styles.clientsWrap}>
-         <div className={styles.header}>
-            <h2 className={styles.topic} >{room?.topic}</h2>
-               <div className={styles.actionBtn}>
-                   <button className={styles.actionBtn}>
-                           <img src="/images/palm.png" alt="palm-icon" />
-                   </button>
-                   <button
-                     onClick={handManualLeave}
-                     className={styles.actionBtn}
-                   >                            
-                     <span >Leave quietly</span>
-                   </button>
-               </div>
-         </div>
-
-         <div className={styles.clientsList}>
-           {uniqueClients.map((client) => {
-             return(
-               <div className={styles.client} 
-                    key={client.id} >
-               <div 
-                     className={styles.userHead}>
-                     
-                 <audio 
-                   ref={(instance) => provideRef(instance, client.id)}
-                     
-                   autoPlay
-                 ></audio>
-                   <img 
-                     className={styles.userAvatar} 
-                     src={client.avatar} 
-                     alt="avatar" />
-
-
-                 <button onClick={()=>handleMuteClick(client.id)} className={styles.micBtn}>
-                  {
-                    client.muted?(
-                        <img 
-                         src="/images/mic-mute.png" 
-                         alt="mic-mute-icon" />
-                    ):(
-                      <img 
-                      src="/images/mic.png" 
-                      alt="mic-icon"/>
-                    )}
-                       
-                 </button>
-                     
-               </div>
-               <h4>{client.name}</h4>
-               </div>
-           )
-           })}
-       </div>
-     </div>
-        }
-     
-
+      <div className={styles.clientsWrap}>
+        <div className={styles.header}>
+          {room && <h2 className={styles.topic}>{room.topic}</h2>}
+          <div className={styles.actions}>
+            <button onClick={handManualLeave} className={styles.actionBtn}>
+              <span>Leave quietly</span>
+            </button>
+          </div>
+        </div>
+        <div className={styles.clientsList}>
+          {uniqueClients.map((client) => (
+            <div className={styles.client} key={client.id}>
+              <div className={styles.userHead}>
+                <img
+                  className={styles.userAvatar}
+                  src={client.avatar}
+                  alt=""
+                />
+                <audio
+                  autoPlay
+                  ref={(instance) => {
+                    provideRef(instance, client.id);
+                  }}
+                />
+                <button
+                  onClick={handleMuteClick}
+                  className={styles.micBtn}
+                  disabled={client.id !== user.id}
+                >
+                  {isMute ? (
+                    <img
+                      className={styles.mic}
+                      src="/images/mic-mute.png"
+                      alt="mic"
+                    />
+                  ) : (
+                    <img
+                      className={styles.micImg}
+                      src="/images/mic.png"
+                      alt="mic"
+                    />
+                  )}
+                </button>
+              </div>
+              <h4>{client.name}</h4>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
